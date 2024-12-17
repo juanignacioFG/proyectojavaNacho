c2f5b35fd6cd4e2b3bfbbd4297756fa37938bbe1
juan ignacio <nho13@hotmail.com>
2024-12-17 11:20:33 +0100

    feat:SQL tables and clases Cliente y Reserva

---
+++ src/main/java/entitiesClass/Cliente.java
@@ -0,0 +1,9 @@
+package entitiesClass;
+
+public class Cliente {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+}
+
---
+++ src/main/java/entitiesClass/Reserva.java
@@ -0,0 +1,10 @@
+package entitiesClass;
+
+import java.time.LocalDateTime;
+
+public class Reserva {
+    private int id;
+    private int clienteId;
+    private LocalDateTime fechaHora;
+    private int numeroComensales;
+}
